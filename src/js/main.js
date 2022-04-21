365f9b171fcabe44167194286ec2310ac26a3ad1
CHARITH127 <charithrullz993@gmail.com>
2022-04-21 15:47:35 +0530

    Add three stages

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ const blueCar = $("#blueCar").get(0);
 const raceCar = $("#raceCar").get(0);
 const game = $("#game").get(0);
 var result = $("#result").get(0);
-const score =  $("#score").get(0);
+const score = $("#score").get(0);
 var count = 0;
 
 /*blue car move*/
@@ -10,7 +10,8 @@ blueCar.addEventListener("animationiteration", function () {
     var random = ((Math.floor(Math.random() * 3)) * 130)
     blueCar.style.left = random + "px";
     count++
-    
+    changeTheAnimationSpeed();
+
 })
 
 /*race car move*/
@@ -22,7 +23,8 @@ window.addEventListener("keydown", function (e) {
             raceCar.style.left = (raceCarLeft + 130) + "px"
         }
 
-    };
+    }
+    ;
 
     /*car moving left side*/
     if (e.key == "ArrowLeft") {
@@ -35,17 +37,37 @@ window.addEventListener("keydown", function (e) {
 })
 
 
-//Game over
+/*game over*/
 function gameOver() {
     var blueCarTop = parseInt(window.getComputedStyle(blueCar).getPropertyValue("top"))
     var blueCarLeft = parseInt(window.getComputedStyle(blueCar).getPropertyValue("left"));
     var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"));
     if ((blueCarLeft === raceCarLeft) && (blueCarTop >= 670)) {
-        result.style.display ="block";
+        result.style.display = "block";
         game.style.display = "none";
-        score.innerHTML =`score : ${count}`
-        count =0;
+        score.innerHTML = `score : ${count}`
+        count = 0;
     }
 }
 
 setInterval(gameOver, 5);
+
+/*stages in game*/
+function changeTheAnimationSpeed() {
+    if (count == 10) {
+        blueCar.style.animationDuration = "2000ms"
+    }else if (count == 20){
+        blueCar.style.animationDuration = "1000ms"
+    }else if (count == 30){
+        blueCar.style.animationDuration = "900ms"
+    }else if (count == 40){
+        blueCar.style.animationDuration = "8000ms"
+    }else if (count>50){
+        blueCar.style.animationDuration = "5000ms"
+    }
+}
+
+/*game restart*/
+$("#btnRestart").click(function () {
+    location.reload();
+});
